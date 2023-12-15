257430bb4b99a3d87b7df915ff89b8dddf375892
AndrewDestacamento <47763383+NekoAlosama@users.noreply.github.com>
2023-12-15 18:39:05 -0500

    10x increase again

--- armor/armor-scripts.js
+++ armor/armor-scripts.js
@@ -100,7 +100,7 @@ class CustomSet {
   }
 
   clear() {
-    this.entries = new Array(10000);
+    this.entries = new Array(100000);
     this.size = 0;
   }
 
@@ -463,8 +463,8 @@ const enchantMaxStats = [0, 0, 0, 0, 0, 0];
 const jewelMaxStats = [0, 0, 0, 0, 0, 0];
 const modifierMaxStats = [0, 0, 0, 0, 0, 0];
 
-const BUILD_SIZE = 1000;
-const ARMOR_SIZE = 5000;
+const BUILD_SIZE = 10000;
+const ARMOR_SIZE = 50000;
 
 let defaultSettings;
 
